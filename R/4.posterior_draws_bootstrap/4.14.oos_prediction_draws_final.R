32e56a0b30007d9236ebddba7c80301f08c55d82
Alyssa Willson <awillso2@nd.edu>
2024-07-17 21:15:07 +0000

    Fixing posterior draws oos prediction

--- R/4.posterior_draws_bootstrap/4.14.oos_prediction_draws_final.R
+++ R/4.posterior_draws_bootstrap/4.14.oos_prediction_draws_final.R
@@ -3,9 +3,9 @@
 ## Out of sample prediction in space and time (using all spatiotemporal
 ## locations not used to fit the model)
 ## This step must be run on VM because of memory constraints
-## Requires ~30 GB RAM
+## Requires ~40 GB RAM
 ## NOTE that this step requires you to change the file path according to what
-## machien you're working on
+## machine you're working on
 ## This is because on a local machine, the files must be saved to an external drive because
 ## of storage constraints, but on the VM, the files cannot be on an external drive and must
 ## be saved somewhere accessible.
@@ -26,3 +26,43 @@
 ## to fit the model
 
 rm(list = ls())
+
+# Load fitted model
+load('out/posteriors/sand_aat_tpr_prsd_300YBP/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP.RData')
+
+# Load out-of-sample data
+load('data/processed/mean_stepps_full_oos.RData')
+
+# Format ydata
+new_ydata <- taxon_oos_all |>
+  dplyr::select(beech:tamarack) |>
+  dplyr::rename(oc = other_conifer,
+                oh = other_hardwood)
+
+# Format xdata (keep all columns to match format from step 4-5)
+xdata <- dplyr::select(taxon_oos_all, clay:prsd)
+
+#### Non-conditional ####
+
+# New data list
+# Number of simulations is dramatically reduced due to extreme
+# computation time. 500 is still a large posterior ensemble,
+# especially since we are only using the predictive mean (no
+# need to characterize the full distribution)
+new_datalist <- list(xdata = xdata,
+                     nsim = 500)
+
+# Storage
+pred <- list()
+
+# Loop over posterior draws
+for(i in 1:100){
+  out <- output[[i]]
+  pred[[i]] <- gjam::gjamPredict(output = out,
+                                 newdata = new_datalist)
+  print(i)
+}
+
+# Save
+save(pred,
+     file = 'out/posteriors/oos_prediction_nonconditional_final.RData')

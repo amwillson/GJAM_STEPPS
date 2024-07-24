2faaa2e5f6f4456b454b3bae36f37ca74e111392
Alyssa Willson <awillso2@nd.edu>
2024-07-24 14:20:38 +0000

    Fixing final OOS prediction with posterior draws

--- R/4.posterior_draws_bootstrap/4.12.oos_prediction_draws_final.R
+++ R/4.posterior_draws_bootstrap/4.12.oos_prediction_draws_final.R
@@ -1,4 +1,4 @@
-### STEP 4-12
+### STEP 4-13
 
 ## Out of sample prediction in space and time (using all spatiotemporal
 ## locations not used to fit the model)
@@ -16,15 +16,15 @@
 ## Input: out/posteriors/sand_aat_tpr_prsd_300YBP/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP.RData
 ## Output from GJAM with sand, aat, tpr, prsd covariates with posterior draws including 300 YBP time step
 
-## Input: data/processed/mean_stepps_full_oos.RData
+## Input: data/processed/post_stepps_full_oos.RData
 ## OOS data for everything not used to fit the model
 
-## Output: out/posteriors/oos_prediction_all.RData
-## Out of sample predictions for non-conditional, conditional, and conditional only on
+## Output: out/posteriors/oos_prediction_nonconditional_final.RData
+## Output: out/posteriors/oos_prediction_conditionaloak_final.RData
+## Out of sample predictions for non-conditional, and conditional only on
 ## oak relative abundances
 ## Out of sample predictions are made for all spatiotemporal locations not used
 ## to fit the model
-## Used in 4.13.plot_oos_prediction_final.R
 
 rm(list = ls())
 
@@ -32,33 +32,31 @@ rm(list = ls())
 load('out/posteriors/sand_aat_tpr_prsd_300YBP/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP.RData')
 
 # Load out-of-sample data
-load('data/processed/mean_stepps_full_oos.RData')
-
-# Format ydata
-new_ydata <- taxon_oos_all |>
-  dplyr::select(beech:tamarack) |>
-  dplyr::rename(oc = other_conifer,
-                oh = other_hardwood)
-
-# Format xdata (keep all columns to match format from step 4-5)
-xdata <- dplyr::select(taxon_oos_all, clay:prsd)
+load('data/processed/post_stepps_full_oos.RData')
 
 #### Non-conditional ####
-
-# New data list
-# Number of simulations is dramatically reduced due to extreme
-# computation time. 500 is still a large posterior ensemble,
-# especially since we are only using the predictive mean (no
-# need to characterize the full distribution)
-new_datalist <- list(xdata = xdata,
-                     nsim = 500)
 
 # Storage
 pred <- list()
 
 # Loop over posterior draws
 for(i in 1:100){
+  # Subset for one posterior draw
+  sub <- dplyr::filter(post_oos_all, draw == i)
+  # Subset for one GJAM fit
   out <- output[[i]]
+  
+  # Format xdata (keep all columns to match format from step 4-5)
+  xdata <- dplyr::select(sub, clay:prsd)
+  
+  # New data list
+  # Number of simulations is dramatically reduced due to extreme
+  # computation time. 500 is still a large posterior ensemble,
+  # especially since we are only using the predictive mean (no
+  # need to characterize the full distribution)
+  new_datalist <- list(xdata = xdata,
+                       nsim = 500)
+  
   pred[[i]] <- gjam::gjamPredict(output = out,
                                  newdata = new_datalist)
   print(i)
@@ -67,3 +65,34 @@ for(i in 1:100){
 # Save
 save(pred,
      file = 'out/posteriors/oos_prediction_nonconditional_final.RData')
+
+#### Conditional on oak ####
+
+rm(pred)
+
+oak_cond_pred <- list()
+
+# Loop over posterior draws
+for(i in 1:100){
+  sub <- dplyr::filter(post_oos_all, draw == i)
+  out <- output[[i]]
+  
+  # Format xdata (keep all columns to match format from step 4-5)
+  xdata <- dplyr::select(sub, clay:prsd)
+  
+  # Format ydata
+  ydata_cond <- dplyr::select(sub, OAK)
+  
+  new_datalist <- list(ydataCond = ydata_cond,
+                       xdata = xdata,
+                       nsim = 500)
+  
+  oak_cond_pred[[i]] <- gjam::gjamPredict(output = out,
+                                          newdata = new_datalist)
+  
+  print(i)
+}
+
+# Save
+save(oak_cond_pred,
+     file = 'out/posteriors/oos_prediction_conditionoak_final.RData')

6b2051d59607a58ac3e831df9c1e992d02019155
amwillson <caleblanesemail@gmail.com>
2025-02-10 22:40:52 -0500

    Lagged climate full prediction

--- R/7.Lagged_climate/7.4.oos_predict_final.R
+++ R/7.Lagged_climate/7.4.oos_predict_final.R
@@ -10,7 +10,19 @@
 rm(list = ls())
 
 # Load fitted model
-load('/Volumes/FileBackup/GJAM_STEPPS_output/posteriors/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP_lagged.RData')
+#load('/Volumes/FileBackup/GJAM_STEPPS_output/posteriors/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP_lagged.RData')
+load('D:/GJAM_STEPPS_post_sand_aat_tpr_prsd_300YBP_lagged.RData')
+
+# Loop through fitted model outputs
+for(i in 1:length(output)){
+  # Save one model run at a time
+  out <- output[[i]]
+  save(out, file = paste('D:/gjam_out_', i, '.RData'))
+  print(i)
+}
+
+# Remove total output
+rm(output, out)
 
 # Load data
 load('data/processed/post_stepps_soil_clim.RData')
@@ -52,3 +64,46 @@ post_oos_all <- post_oos_all |>
                                      aatLag, tprLag, prsdLag),
                    by = c('x', 'y', 'draw', 'time'))
 
+# Loop over posterior draws
+for(i in 1:100){
+  # Subset for one posterior draw
+  sub <- post_oos_all |>
+    dplyr::filter(draw == i) |>
+    tidyr::drop_na()
+  
+  # Subset for one GJAM fit
+  load(paste0('D:/gjam_out_', i, '.RData'))
+  
+  # Format xdata (keep all columns to match format from step 7-3)
+  xdata <- dplyr::select(sub, clay:prsdLag)
+  
+  # New data list
+  # Number of simulations is dramatically reduced due to extreme
+  # computation time. 500 is still a large posterior ensemble,
+  # especially since we are only using the predictive mean (no
+  # need to characterize the full distribution)
+  new_datalist <- list(xdata = xdata,
+                       nsim = 500)
+  
+  pred <- gjam::gjamPredict(output = out,
+                            newdata = new_datalist)
+  
+  save(pred, file = paste0('D:/pred_', i, '.RData'))
+  
+  print(i)
+}
+
+# Save object
+pred_all <- list()
+
+# Loop through all the predictions
+for(i in 1:100){
+  # Open one prediction run
+  load(paste0('D:/pred_', i, '.RData'))
+  pred_all[[i]] <- pred
+  print(i)
+}
+
+# Save
+save(pred_all,
+     file = 'D:/oos_prediction_nonconditional_final_lagged.RData')
